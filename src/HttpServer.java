bb2ffc80bd17fa7fded69e4a335cde85afc1ab4a
Leonardo <8293569@itisrossi.vi.it>
2024-01-18 10:20:01 +0100

    non va ma si può fixare

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -9,52 +9,64 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer {
-    public static void main(String[] args) {
+    private static ServerSocket ss;
+    private static Socket s;
+    public static void main(String[] args) throws IOException {
         System.out.println("Server Starting...");
-
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/configuration/conf.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
         System.out.println("Using port: " + conf.getPort());
         System.out.println("Using WebRoot: " + conf.getWebroot());
 
-        try {
-            ServerSocket ss = new ServerSocket(8080);
-            Socket s = ss.accept();
+        ss = new ServerSocket(8080);
+        Thread thread = new Thread(() -> {
+            try {
+                sendOutput();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
-            System.out.println("new client");
-
-            InputStream is = s.getInputStream();
-            OutputStream os = s.getOutputStream();
-
-            String html =
-                    //BODY
-                    "<!DOCTYPE html>\n" +
-                    "<html lang=\"en\">\n" +
-                    "<head>\n" +
-                    "    <meta charset=\"UTF-16\">\n" +
-                    "    <title>Web Server</title>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "<h1>Home Page</h1>" +
-                    "</body>\n" +
-                    "</html>";
-
-            String response =
-                    //STATUS
-                    "HTTP/1.1 200 OK\r\n" +
-                    //HEADER
-                    "Content-length: " + html.getBytes().length + "\r\n\r\n" +
-                    html;
-
-            os.write(response.getBytes());
-
-            os.close();
-            is.close();
-            s.close();
-            ss.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        while (true) {
+            System.out.println("now accepting...");
+            s = ss.accept();
+            System.out.println("starting thread...");
+            thread.start();
+            System.out.println("thread finished...");
         }
+    }
+
+    public static synchronized void sendOutput() throws IOException {
+        System.out.println("new client");
+
+        InputStream is = s.getInputStream();
+        OutputStream os = s.getOutputStream();
+
+        String html =
+                //BODY
+                "<!DOCTYPE html>\n" +
+                        "<html lang=\"en\">\n" +
+                        "<head>\n" +
+                        "    <meta charset=\"UTF-16\">\n" +
+                        "    <title>Web Server</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "<h1>Home Page</h1>" +
+                        "</body>\n" +
+                        "</html>";
+
+        String response =
+                //STATUS
+                "HTTP/1.1 200 OK\r\n" +
+                        //HEADER
+                        "Content-length: " + html.getBytes().length + "\r\n\r\n" +
+                        html;
+
+        os.write(response.getBytes());
+
+        os.close();
+        is.close();
+        s.close();
     }
 }
